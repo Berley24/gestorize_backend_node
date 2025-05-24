d8bffdc0fc02256d8c424a39a5b5f89c1e12acd4
Berley24 <158833529+Berley24@users.noreply.github.com>
2025-05-24 08:11:38 -0300

    deploy8

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+const API = import.meta.env.VITE_API_URL;
+
 const path = require("path");
 
 const app = express();
